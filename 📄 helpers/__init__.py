07d073b7aa98455ed6ef865930ce928c38886c7f
Awelama <awelamakwarase@gmail.com>
2025-04-12 07:48:36 -0700

    Create __init__.py

---
+++ 📄 helpers/__init__.py
@@ -0,0 +1,6 @@
+def recommend_resources(context):
+    return [
+        "BetterEvaluation.org – Guide to Evaluation Strategies",
+        "UNDP Handbook on Monitoring and Evaluation",
+        "OECD-DAC Criteria for Evaluating Development Assistance"
+    ]
