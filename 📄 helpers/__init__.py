97182651c57df251321043dbc0efec559c715cdb
Awelama <awelamakwarase@gmail.com>
2025-04-12 08:09:39 -0700

    Delete 📄 helpers directory

--- 📄 helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-def recommend_resources(context):
-    return [
-        "BetterEvaluation.org – Guide to Evaluation Strategies",
-        "UNDP Handbook on Monitoring and Evaluation",
-        "OECD-DAC Criteria for Evaluating Development Assistance"
-    ]
